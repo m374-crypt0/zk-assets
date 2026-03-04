aae941e13c68c3e779b79a0edb7b0113f7364c58
Sebastien Levy <m374-crypt0@pm.me>
2026-03-04 10:19:22 +0000

    test(customer): commitment creation
    created a first unit test to ensurer a poseidon2 commitment can be
    created

--- customer/src/index.ts
+++ customer/src/index.ts
@@ -1,1 +1,33 @@
-console.log("Hello via Bun!");
+import { Barretenberg, Fr } from "@aztec/bb.js"
+
+export type CreateCommitmentOptions = {
+  customerId: number,
+  policy: {
+    id: number,
+    scope: {
+      id: number,
+      parameters: {
+        [name: string]: unknown
+      }
+    }
+  },
+  evmAddress: bigint,
+  secret: bigint
+}
+
+export default {
+  async createCommitment(options: CreateCommitmentOptions) {
+    const bb = await Barretenberg.new()
+
+    const hash = await bb.poseidon2Hash([
+      new Fr(BigInt(options.customerId)),
+      new Fr(options.secret),
+      new Fr(options.evmAddress),
+      new Fr(BigInt(options.policy.id)),
+      new Fr(BigInt(options.policy.scope.id)),
+      new Fr(options.policy.scope.parameters.validUntil as bigint)
+    ])
+
+    return BigInt(hash.toString())
+  }
+}
--- customer/test/index.test.ts
+++ customer/test/index.test.ts
@@ -1,7 +1,43 @@
-import { expect, describe, it, } from "bun:test";
+import customer, { type CreateCommitmentOptions } from "src"
 
-describe('dummy section', () => {
-  it('should just not work', () => {
-    expect(true, 'Test not implemented').toBeFalse()
+import { Barretenberg, Fr } from "@aztec/bb.js"
+
+import { expect, describe, it, beforeEach, } from "bun:test";
+
+describe('Commitment creation', () => {
+  let bb: Barretenberg
+
+  beforeEach(async () => {
+    bb = await Barretenberg.new()
+  })
+
+  it('should be able to create a poseidon2 commitment from input', async () => {
+    const data: CreateCommitmentOptions = {
+      customerId: 1,
+      evmAddress: 0n,
+      policy: {
+        id: 0,
+        scope: {
+          id: 0,
+          parameters: {
+            validUntil: 0n
+          }
+        }
+      },
+      secret: BigInt(Fr.random().toString())
+    }
+
+    const expectedCommitment = await bb.poseidon2Hash([
+      new Fr(BigInt(data.customerId)),
+      new Fr(data.secret),
+      new Fr(data.evmAddress),
+      new Fr(BigInt(data.policy.id)),
+      new Fr(BigInt(data.policy.scope.id)),
+      new Fr(data.policy.scope.parameters.validUntil as bigint)
+    ])
+
+    const commitment = await customer.createCommitment(data)
+
+    expect(commitment).toBe(BigInt(expectedCommitment.toString()))
   })
 })
