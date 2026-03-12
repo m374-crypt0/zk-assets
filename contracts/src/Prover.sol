b94f5d1b3a26c1ccf30bf53a35f735acf0ea5ed6
Sebastien Levy <m374-crypt0@pm.me>
2026-03-12 09:13:11 +0000

    refactor(contracts): more typed stuff in Prover.sol

--- contracts/src/Prover.sol
+++ contracts/src/Prover.sol
@@ -5,6 +5,15 @@ import { IVerifier } from "./interfaces/IVerifier.sol";
 
 import { CommitmentStore } from "./CommitmentStore.sol";
 
+struct PublicInputs {
+  bytes32 policyId;
+  bytes32 policyScopeId;
+  bytes32[] policyScopeParameters;
+  bytes32 _unused1;
+  bytes32 _unused2;
+  bytes32 commitment;
+}
+
 contract Prover {
   error InvalidCommitment();
   error InvalidProof();
@@ -17,13 +26,19 @@ contract Prover {
     verifier = verifier_;
   }
 
-  function prove(bytes calldata zkp_, bytes32[] calldata publicInputs_) external view returns (bool) {
+  function prove(bytes calldata zkp_, PublicInputs calldata publicInputs_) external view returns (bool) {
     // NOTE: see ../../circuits/src/main.nr, function main arguments
-    uint256 commitment = uint256(publicInputs_[5]);
+    uint256 commitment = uint256(publicInputs_.commitment);
 
     require(store.commitments(commitment), InvalidCommitment());
 
-    try verifier.verify(zkp_, publicInputs_) returns (bool result) {
+    bytes32[] memory verifierInputs = new bytes32[](6);
+    verifierInputs[0] = publicInputs_.policyId;
+    verifierInputs[1] = publicInputs_.policyScopeId;
+    verifierInputs[2] = publicInputs_.policyScopeParameters[0];
+    verifierInputs[5] = publicInputs_.commitment;
+
+    try verifier.verify(zkp_, verifierInputs) returns (bool result) {
       require(result, InvalidProof());
       return result;
     } catch (bytes memory) {
--- contracts/test/Prover.t.sol
+++ contracts/test/Prover.t.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.33;
 import { IPrimeFieldOrderProvider } from "../src/interfaces/IPrimeFieldOrderProvider.sol";
 
 import { CommitmentStore } from "../src/CommitmentStore.sol";
-import { Prover } from "../src/Prover.sol";
+import { Prover, PublicInputs } from "../src/Prover.sol";
 
 import { PrimeFieldOrderProvider } from "./stubs/PrimeFieldOrderProvider.sol";
 import { Verifier } from "./stubs/Verifier.sol";
@@ -23,19 +23,17 @@ contract ProverTests is Test {
 
   uint256 constant COMMITMENT = 2;
   bytes public zkpStub;
-  bytes32[] public publicInputsStub;
+  PublicInputs public publicInputsStub;
 
   function setUp() public {
     issuer = makeAddr("issuer");
     customer = makeAddr("customer");
 
-    // NOTE: see ../../circuits/src/main.nr, function main arguments
-    publicInputsStub.push(0);
-    publicInputsStub.push(0);
-    publicInputsStub.push(0);
-    publicInputsStub.push(0);
-    publicInputsStub.push(0);
-    publicInputsStub.push(bytes32(COMMITMENT));
+    publicInputsStub.policyId = 0;
+    publicInputsStub.policyScopeId = 0;
+    publicInputsStub.policyScopeParameters = new bytes32[](1);
+    publicInputsStub.policyScopeParameters[0] = 0;
+    publicInputsStub.commitment = bytes32(COMMITMENT);
 
     pfop = new PrimeFieldOrderProvider();
     store = new CommitmentStore(pfop, issuer);
