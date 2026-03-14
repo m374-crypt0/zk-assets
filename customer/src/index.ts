fd79c68ef9d850124cb882aad526408cbc3e8be9
Sebastien Levy <m374-crypt0@pm.me>
2026-03-14 09:00:09 +0000

    chore(circuits): rebranding

--- customer/src/index.ts
+++ customer/src/index.ts
@@ -8,7 +8,7 @@ import { toHex } from "viem"
 import { type OnChainProver, type ProverInputs } from "./blockchain/types/onChainProver"
 import type { CommitmentInputs, Inputs, PublicInputs } from "./types"
 
-import circuit from "circuits/target/zhold_v1.json"
+import circuit from "circuits/target/zk_assets_v1.json"
 
 export default {
   async createCommitment(options: CommitmentInputs) {
