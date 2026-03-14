62ec1d0a92d4c5c4d48ebb344713904ef5a6fba3
Sebastien Levy <m374-crypt0@pm.me>
2026-03-14 08:34:29 +0000

    chore(contracts): updated solidity compiler
    from 0.8.33 to 0.8.34

--- contracts/src/interfaces/IPrimeFieldOrderProvider.sol
+++ contracts/src/interfaces/IPrimeFieldOrderProvider.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.33;
+pragma solidity ^0.8.0;
 
 interface IPrimeFieldOrderProvider {
   function P() external pure returns (uint256);
--- contracts/src/interfaces/IVerifier.sol
+++ contracts/src/interfaces/IVerifier.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.33;
+pragma solidity ^0.8.0;
 
 interface IVerifier {
   function verify(bytes calldata proof_, bytes32[] calldata publicInputs_) external view returns (bool);
