7a02394aa9865b2f3ab7bbfcb2379de8577df989
crypt0 <m374-crypt0@pm.me>
2026-08-10 08:02:29 +0000

    chore: updated to solidity 0.8.36
    Without any hiccup, all tests green

--- contracts/script/SepoliaDeploy.s.sol
+++ contracts/script/SepoliaDeploy.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.35;
+pragma solidity 0.8.36;
 
 import { Script } from "forge-std/Script.sol";
 
--- contracts/test/stubs/Verifier.sol
+++ contracts/test/stubs/Verifier.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.35;
+pragma solidity 0.8.36;
 
 import { IVerifier } from "../../src/interfaces/IVerifier.sol";
 
